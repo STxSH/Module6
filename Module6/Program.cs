0c2d8837085dc1216c6057e110ac68cc9bf13822
STxSH <anton.batalov94@gmail.com>
2023-09-15 23:58:29 +0700

    task 6.2.2 done

--- Module6/Program.cs
+++ Module6/Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+
+        //task 6.2.2
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pen pen = new Pen("Синий", 25);
+
+            Console.WriteLine($"цвет: {pen.color}, цена: {pen.cost}");
+            Console.ReadKey();
+        }
+
+        class Pen
+        {
+            public string color;
+            public int cost;
+
+            public Pen()
+            {
+                color = "Черный";
+                cost = 10;
+            }
+
+            public Pen(string penColor, int penCost)
+            {
+                color = penColor;
+                cost = penCost;
+            }
         }
     }
 }
