5666b9cc3fbcaa53aee0221625a0e1c5df60f675
Anton <anton.batalov94@gmail.com>
2023-09-26 16:58:39 +0700

    task 6.6.1 done

--- Module6/Program.cs
+++ Module6/Program.cs
@@ -1,54 +1,26 @@
-﻿namespace Module6
+﻿using System.Drawing;
+
+namespace Module6
 {
     internal class Program
     {
-        //task 6.5.2
+        //task 6.6.1
         static void Main(string[] args)
         {
             Console.ReadKey();
         }
     }
 
-    class Circle
+    class TrafficLight
     {
-        public double radius;
-
-        public double Area()
+        private void ChangeColor(string color)
         {
-            return 0;
+
         }
 
-        public double Perimeter()
+        public string GetColor()
         {
-            return 0;
-        }
-    }
-
-    class Triangle
-    {
-        public double a, b, c;
-        public double Area()
-        {
-            return 0;
-        }
-
-        public double Perimeter()
-        {
-            return 0;
-        }
-    }
-
-    class Square
-    {
-        public double a;
-        public double Area()
-        {
-            return 0;
-        }
-
-        public double Perimeter()
-        {
-            return 0;
+            return "color";
         }
     }
 }
