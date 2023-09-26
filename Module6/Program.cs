45c2a5f181772f21f71192c36d559462d5b18d8a
Anton <anton.batalov94@gmail.com>
2023-09-26 17:07:58 +0700

    task 6.6.2 done

--- Module6/Program.cs
+++ Module6/Program.cs
@@ -4,23 +4,77 @@ namespace Module6
 {
     internal class Program
     {
-        //task 6.6.1
+        //task 6.6.2
         static void Main(string[] args)
         {
             Console.ReadKey();
         }
     }
 
-    class TrafficLight
+    class User
     {
-        private void ChangeColor(string color)
-        {
+        private int age;
+        private string login;
+        private string email;
 
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
         }
 
-        public string GetColor()
+        public string Login
         {
-            return "color";
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                if (value.Length >= 3)
+                {                   
+                    login = value;
+                }
+                else
+                {
+                    Console.WriteLine("Поле логина должно быть не менее 3 символов длиной.");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                if (value.Contains('@'))
+                {
+                    email = value;
+                }
+                else
+                {
+                    Console.WriteLine("Поле почты должно содержать знак @.");
+                }
+            }
         }
     }
 }
